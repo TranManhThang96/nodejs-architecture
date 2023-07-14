b21602acca96b263185834881a0c2981e1220d2c
TranManhThang <tranmanhthang96@gmail.com>
2023-07-14 17:03:27 +0700

    prettier

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require('./src/app');
+const app = require("./src/app");
 
 const PORT = 3000;
 
@@ -8,9 +8,9 @@ const server = app.listen(PORT, () => {
 
 // SIGINT (tín hiệu bàn phím interrupt)
 // SIGINT khi người dùng nhấn phím Ctrl + C trên bàn phím để yêu cầu dừng quá trình đó
-process.on('SIGINT', () => {
+process.on("SIGINT", () => {
   server.close(() => {
-    console.log('Exit Server Express');
+    console.log("Exit Server Express");
     // notify.send(ping...)
   });
 });
--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const compression = require('compression');
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const compression = require('compression')
 
-const app = express();
+const app = express()
 
 // init middleware
-app.use(morgan('dev')); // khi dev thì nên bật mode này
-app.use(helmet());
-app.use(compression());
+app.use(morgan('dev')) // khi dev thì nên bật mode này
+app.use(helmet())
+app.use(compression())
 
 // init db
 
@@ -17,10 +17,10 @@ app.get('/', (req, res, next) => {
   return res.status(200).json({
     msg: 'oki',
     metadata: 'compression'.repeat(100000),
-  });
-});
+  })
+})
 
 // handling error
-app.get('/', (err, req, res, next) => {});
+app.get('/', (err, req, res, next) => {})
 
-module.exports = app;
+module.exports = app
